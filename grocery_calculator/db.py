7340dfb2f60aa8db08282ec019b13a43a5da2ddf
Jon Johnson <jonjohnsontc@live.com>
2025-01-20 10:59:00 -0800

    adding db scaffolding

--- grocery_calculator/db.py
+++ grocery_calculator/db.py
@@ -1,10 +1,21 @@
+import os
 import duckdb
+
+from typing import List, Any
+
+CONN_STR = os.getenv("DB_CONN_STR")
 
 
 class Database:
 
     def connect(self) -> None:
-        pass
+        if not CONN_STR:
+            raise OSError("Database connection string 'DB_CONN_STR' not found")
+        self.con = duckdb.connect(CONN_STR)
 
-    def execute_query(self, text: str) -> None:
-        pass
+    def execute_query(self, text: str) -> List[Any]:
+        if not self.con:
+            raise ConnectionError(
+                "Not yet connected to database. Have you tried running 'connect'?"
+            )
+        return self.con.sql(text).fetchall()
