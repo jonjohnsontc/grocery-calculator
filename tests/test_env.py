47b848d106f08467f7c22ebfb162585ef86123b3
Jon Johnson <jonjohnsontc@live.com>
2025-01-20 10:29:36 -0800

    adding some test scaffolding

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,14 +1,15 @@
 import os
 import unittest
-
-from pathlib import Path
+import logging
 
 from grocery_calculator.env import load_env_file
+from tests import FIXTURES_DIR
+
+
+FILE = f"{FIXTURES_DIR}/test.env"
 
 
 class TestEnv(unittest.TestCase):
-
-    FILE = Path(__file__).parent.joinpath("fixtures", "test.env").as_posix()
 
     def setUp(self):
         self.original_env = os.environ.copy()
@@ -18,11 +19,21 @@ class TestEnv(unittest.TestCase):
         os.environ.update(self.original_env)
 
     def test_load_env_file_loads_all_vars(self):
-        load_env_file(self.FILE)
+        load_env_file(FILE)
         self.assertIn("KEY1", os.environ)
         self.assertIn("ALREADY_EXISTS", os.environ)
 
     def test_load_env_doesnt_overwrite_already_set_vars(self):
         os.environ["ALREADY_EXISTS"] = "now_set"
-        load_env_file(self.FILE)
+
+        # we want to surpress any warnings from the logger
+        logger = logging.getLogger()
+        prev_level = logger.level
+        logger.setLevel(logging.ERROR)
+
+        try:
+            load_env_file(FILE)
+        finally:
+            logger.setLevel(prev_level)
+
         self.assertEqual(os.environ.get("ALREADY_EXISTS"), "now_set")
