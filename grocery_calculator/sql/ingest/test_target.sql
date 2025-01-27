1f59676ebc30848c4e27c534e2bf5c67385a96f4
Jon Johnson <jonjohnsontc@live.com>
2025-01-26 17:12:16 -0800

    adding comment to target test script

--- grocery_calculator/sql/ingest/test_target.sql
+++ grocery_calculator/sql/ingest/test_target.sql
@@ -1,2 +1,4 @@
+-- I've been running the following files to test running the ingest
+-- pipeline for Target scraped data
 .read grocery_calculator/sql/seed.sql
 .read grocery_calculator/sql/ingest/copy_target.sql
