ec1a292032c0b423a1d62dcce5bdf16718f2a91c
Jon Johnson <jonjohnsontc@live.com>
2025-01-29 06:53:41 -0800

    checkpoint

--- tests/ingest/test_target_ingest.py
+++ tests/ingest/test_target_ingest.py
@@ -1,3 +1,8 @@
-from tests import FIXTURES_DIR
+import unittest
 
+from tests import FIXTURES_DIR
 from grocery_calculator.ingest.target import TargetIngest
+
+
+class TestTargetIngest(unittest.TestCase):
+    pass
