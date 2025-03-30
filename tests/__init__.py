32d24b32d4c5b97a1d6478bbca4949ce1e25101b
Jon Johnson <jonjohnsontc@live.com>
2025-03-30 13:30:19 -0700

    suppress logs with helper func on tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,15 @@
+from contextlib import contextmanager
 from pathlib import Path
 
+import logging
+
 FIXTURES_DIR = Path(__file__).parent.joinpath("fixtures").as_posix()
+
+
+@contextmanager
+def suppress_logging():
+    logging.disable(logging.CRITICAL)
+    try:
+        yield
+    finally:
+        logging.disable(logging.NOTSET)
--- tests/ingest/stores/test_target_ingest.py
+++ tests/ingest/stores/test_target_ingest.py
@@ -1,8 +1,7 @@
-import logging
-import logging.handlers
 import unittest
 import unittest.mock
 
+from tests import suppress_logging
 from grocery_calculator.ingest.stores.target import TargetIngest
 
 
@@ -19,25 +18,22 @@ class TestTargetIngest(unittest.TestCase):
             "sale_value": None,
             "tags": ["Ice Cream", "Frozen", "Fruit Cake"],
         }
-
-        ti = TargetIngest()
-        ti._tag_item = unittest.mock.MagicMock(return_value=TEST_TAGGED_ITEM)
-        logger = logging.getLogger()
-        logger.addHandler(logging.NullHandler())
-        items = [
-            (
-                1,
-                "favorite day | blueberry streusel muffins - 4ct/14oz - favorite day™ |",
-            ),
-            (
-                2,
-                "once upon a farm | save 15% in cart on select once upon a farm foods | once upon a farm strawberry banana swirl organic dairy-free kids' smoothie - 4ct/4oz pouches",
-            ),
-            (
-                3,
-                "daily harvest | daily harvest frozen banana and almond smoothie - 7.4oz |",
-            ),
-        ]
-        tagged = ti.tag_items(items)
+        with suppress_logging():
+            ti = TargetIngest()
+            ti._tag_item = unittest.mock.MagicMock(return_value=TEST_TAGGED_ITEM)
+            items = [
+                (
+                    1,
+                    "favorite day | blueberry streusel muffins - 4ct/14oz - favorite day™ |",
+                ),
+                (
+                    2,
+                    "once upon a farm | save 15% in cart on select once upon a farm foods | once upon a farm strawberry banana swirl organic dairy-free kids' smoothie - 4ct/4oz pouches",
+                ),
+                (
+                    3,
+                    "daily harvest | daily harvest frozen banana and almond smoothie - 7.4oz |",
+                ),
+            ]
+            tagged = ti.tag_items(items)
         self.assertEqual(len(tagged), 3)
-        logger.removeHandler(logger.handlers[0])
